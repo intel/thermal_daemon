683b9b1a22d0beb91971720d844760e6c6479c01
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2013-01-24 10:09:27 -0800

    Simplified implementation of model
    Simplified implementation for learning set points.
    
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

--- src/thd_model.cpp
+++ src/thd_model.cpp
@@ -36,16 +36,9 @@ cthd_model::cthd_model()
 	: trend_increase_start(0),
 	  max_temp(def_max_temperature),
 	  set_point(def_max_temperature),
-	  start_time(0),
 	  last_temp(0),
-	  num_samples(0),
-	  current_total(0),
-	  last_moving_average(0),
-	  begin_moving_average(0),
-	  current_moving_average(0),
-	  trend_increase(0),
 	  max_temp_reached(0),
-	  current_angle(def_low_angle_from_max),
+	  current_angle(0),
 	  set_point_reached(false),
 	  delay_cnt(0),
 	  max_temp_seen(false)
@@ -54,60 +47,52 @@ cthd_model::cthd_model()
 
 unsigned int cthd_model::update_set_point()
 {
+
 	double slope;
 	double delta_x = (max_temp_reached - trend_increase_start) * 1000;
-	double delta_y = current_moving_average -  begin_moving_average;
+	double delta_y = max_temp -  hot_zone;
 	unsigned int _setpoint;
 	double radians;
 	double arc_len;
 
-	if (delta_y > 0) {
+	if (delta_y > 0 && delta_x > 0) {
 		slope = delta_y /delta_x;
 		radians = atan(slope);
-		thd_log_debug("current slope %g angle before %g (%g degree)\n", slope, radians,  57.2957795 * radians);
-
-		radians += (0.01746 * angle_increment);
-		thd_log_debug("current slope %g angle before %g (%g degree)\n", slope, radians,  57.2957795 * radians);
-
-		arc_len = delta_x * tan(radians) ;
+		thd_log_info("current slope %g angle before %g (%g degree)\n", slope, radians,  57.2957795 * radians);
+		radians += (0.01746 * (current_angle+angle_increment));
+		thd_log_info("current slope %g angle before %g (%g degree)\n", slope, radians,  57.2957795 * radians);
+		arc_len = delta_x * tan(radians);
 		_setpoint = max_temp - (unsigned int)(arc_len - delta_y);
-
-		thd_log_debug("** set point  x:%g y:%g arc_len:%g set_point %u\n", delta_x, delta_y, arc_len, _setpoint);
-
+		thd_log_info("** set point  x:%g y:%g arc_len:%g set_point %u\n", delta_x, delta_y, arc_len, _setpoint);
 		if (abs(set_point - _setpoint) > max_compensation)
-				set_point -= max_compensation;
+			set_point -= max_compensation;
 		else
 			set_point = _setpoint;
+		current_angle += angle_increment;
 	}
+
 	return set_point;
 }
 
 void cthd_model::add_sample(unsigned int temperature)
 {
-
 	time_t tm;
-	static int trend_show = 0;
-
-	last_temp = temperature;
-
 
 	time(&tm);
-	if (!start_time)
-		start_time = tm;
-
-	current_moving_average = moving_average(temperature);
-
+	if (trend_increase_start==0 && temperature > hot_zone) {
+		trend_increase_start = tm;
+		thd_log_debug("Trend increase start %ld\n",  trend_increase_start);
+	} else if (trend_increase_start && temperature < hot_zone) {
+		thd_log_debug("Trend increase stopped %ld\n",  trend_increase_start);
+		trend_increase_start = 0;
+	}
 	if (temperature >= max_temp) {
-
 		max_temp_reached = tm;
-
 		// Very first time when we reached max temp
 		// then we need to start tuning
 		if (!max_temp_seen) {
 			unsigned int _set_point;
-
 			update_set_point();
-
 			_set_point = read_set_point();
 			// Update only if the current set point is more than
 			// the stored one.
@@ -115,40 +100,17 @@ void cthd_model::add_sample(unsigned int temperature)
 				store_set_point();
 			max_temp_seen = true;
 		}
-
 		// Give some time to cooling device to cool, after that set next set point
-		if (delay_cnt > def_setpoint_delay_cnt)
+		if (delay_cnt > def_setpoint_delay_cnt && (last_temp < temperature))
 			update_set_point();
 		delay_cnt++;
-
 		set_point_reached = true;
 	} else {
 		set_point_reached = false;
 		delay_cnt = 0;
 	}
-
-	if(last_moving_average && (current_moving_average - last_moving_average) > trend_increase_offset) {
-		if (!trend_increase) {
-			trend_increase = 1;
-			begin_moving_average = current_moving_average;
-			trend_show = current_moving_average;
-			trend_increase_start =  tm;
-		}
-	}
-
-	if(last_moving_average && (last_moving_average - current_moving_average) > trend_increase_offset) {
-		if (trend_increase) {
-			trend_increase = 0;
-			trend_show = current_moving_average;
-			begin_moving_average = current_moving_average;
-			trend_increase_start = tm;
-		}
-	}
-
-
-	last_moving_average = current_moving_average;
-
-	thd_log_debug("sample, Moving average, %u, %u, %d\n", temperature, current_moving_average, trend_show);
+	last_temp = temperature;
+	thd_log_info("update_set_point %u,%d,%u\n", last_temp, current_angle, set_point);
 }
 
 void cthd_model::store_set_point()
@@ -157,7 +119,6 @@ void cthd_model::store_set_point()
 	std::ofstream file;
 
 	filename << TDCONFDIR << "/" << "thermal_set_point.conf";
-
 	std::ofstream fout(filename.str().c_str());
 	if (fout.good()){
         fout << set_point;
@@ -172,13 +133,11 @@ unsigned int cthd_model::read_set_point()
 	unsigned int _set_point = 0;
 
 	filename << TDCONFDIR << "/" << "thermal_set_point.conf";
-
 	std::ifstream ifs(filename.str().c_str(), std::ifstream::in);
 	if (ifs.good()){
 		ifs >> _set_point;
 	}
 	ifs.close();
-
 	thd_log_info("Read set point %u\n", _set_point);
 	return _set_point;
 }
@@ -194,4 +153,5 @@ void cthd_model::set_max_temperature(unsigned int temp)
 	} else {
 		set_point = max_temp;
 	}
+	hot_zone = max_temp - ((max_temp*hot_zone_percent)/100);
 }
--- src/thd_model.h
+++ src/thd_model.h
@@ -29,60 +29,29 @@ class cthd_model {
 
 private:
 	static const int def_max_temperature = 100*1000;
-	static const int mov_average_window = 5;
-	static const int trend_increase_offset = 1000;
 	static const unsigned int safety_margin = 3*1000;
-	static const int def_low_angle_from_max = 5;
 	static const int angle_increment = 2;
-	static const int def_setpoint_delay_cnt = 8;
+	static const int def_setpoint_delay_cnt = 3;
 	static const unsigned int max_compensation = 5000;
+	static const unsigned int hot_zone_percent = 20; //20%
 
-	void add(unsigned int value)
-	{
-		if (num_samples < mov_average_window) {
-			samples[num_samples++] = value;
-			current_total += value;
-		} else {
-			unsigned int& oldest = samples[num_samples++ % mov_average_window];
-			current_total += value - oldest;
-			oldest = value;
-		}
-	}
-
-	unsigned int moving_average(unsigned int value)
-	{
-		add(value);
-		return current_total / std::min(num_samples, mov_average_window);
-	}
-
-	unsigned int samples[mov_average_window];
-	int num_samples;
-	unsigned int current_total;
 
 	unsigned int max_temp;
+	unsigned int hot_zone;
 	unsigned int last_temp;
 
-	time_t	start_time;
 	time_t trend_increase_start;
 	time_t trend_decrease_start;
-
 	time_t max_temp_reached;
 
 	unsigned int set_point;
-	unsigned int set_point_next;
-
-	int	trend_increase;
-	int begin_moving_average;
-	int current_moving_average;
-	int last_moving_average;
 
 	int current_angle;
-	bool set_point_reached;
 	int delay_cnt;
 	bool max_temp_seen;
-
+	bool set_point_reached;
+		
 	unsigned int update_set_point();
-
 	void store_set_point();
 	unsigned int read_set_point();
 
@@ -93,5 +62,4 @@ public:
 	void set_max_temperature(unsigned int temp);
 	unsigned int get_set_point() { return set_point; }
 	bool is_set_point_reached() { return set_point_reached; };
-	unsigned int get_moving_average() { return current_moving_average; }
 };
