fa20693b3d6a4fb1914deec909313803b3ffb509
Radivoje Jovanovic <radivoje.jovanovic@intel.com>
2015-10-05 15:13:40 -0700

    change the makefile to point to the correct location for config files
    Signed-off-by: Radivoje Jovanovic <radivoje.jovanovic@intel.com>
    Reviewed-by: Leung, Daniel <daniel.leung@intel.com>

--- Android.mk
+++ Android.mk
@@ -45,7 +45,7 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH) $(thermald_src_path) \
 			system/core/include/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS := -fpermissive -DTDRUNDIR='"data/thermal-daemon"' -DTDCONFDIR='"system/etc/thermal-daemon"'
+LOCAL_CFLAGS := -fpermissive -DTDRUNDIR='"/data/thermal-daemon"' -DTDCONFDIR='"/system/etc/thermal-daemon"'
 LOCAL_STATIC_LIBRARIES := libxml2
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libstlport libicuuc libicui18n
 LOCAL_PRELINK_MODULE := false
