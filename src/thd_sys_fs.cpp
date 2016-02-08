c2e17ee973c59ca2b0898a147fa059c0c32369f4
Colin Ian King <colin.king@canonical.com>
2016-02-08 11:35:51 +0000

    csys_fs::write: return -errno on write failure
    sysfs writes can fail for various reasons, and returning -errno is
    useful for indicating an error so that the caller can determine
    the best strategy to take if a sysfs write fails.  This change
    is compatible with the current semantics, e.g. -ve is an error, but
    also propagates the errno back to the caller.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/thd_sys_fs.cpp
+++ src/thd_sys_fs.cpp
@@ -34,8 +34,10 @@ int csys_fs::write(const std::string &path, const std::string &buf) {
 		return -errno;
 	}
 	int ret = ::write(fd, buf.c_str(), buf.size());
-	if (ret < 0)
+	if (ret < 0) {
+		ret = -errno;
 		thd_log_warn("sysfs write failed %s\n", path.c_str());
+	}
 	close(fd);
 
 	return ret;
