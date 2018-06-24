644114fe343c22dd800483cb5ea8d808c7b6d6d6
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2018-06-23 17:27:43 -0700

    Add option ignore-default-control for Android

--- src/android_main.cpp
+++ src/android_main.cpp
@@ -182,6 +182,7 @@ int main(int argc, char *argv[]) {
 			{ "test-mode", no_argument, 0, 't' },
 			{ "config-file", required_argument, 0, 'c' },
 			{ "ignore-cpuid-check", no_argument, 0, 'i'},
+			{ "ignore-default-control", no_argument, 0, 'd'},
 			{ NULL, 0, NULL, 0 } };
 
 	if (argc > 1) {
@@ -212,6 +213,9 @@ int main(int argc, char *argv[]) {
 				break;
 			case 'i':
 				ignore_cpuid_check = true;
+				break;
+			case 'd':
+				thd_ignore_default_control = true;
 				break;
 			case -1:
 			case 0:
