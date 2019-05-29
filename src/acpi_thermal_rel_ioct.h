ecf2487cd040354d0def1010b840536e2ba38950
Zhen Han <zhen.han@intel.com>
2019-05-29 18:15:52 +0800

    Add the license head of acpi_thermal_rel_ioct.h
    Signed-off-by: Zhen Han <zhen.han@intel.com>

--- src/acpi_thermal_rel_ioct.h
+++ src/acpi_thermal_rel_ioct.h
@@ -1,9 +1,29 @@
+/*
+ * acpi_thermal_rel_ioct.h: head file of _ART and _TRT objects
+ *
+ * Copyright (C) 2012 Intel Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 or later as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
 #ifndef __ACPI_ACPI_THERMAL_H
 #define __ACPI_ACPI_THERMAL_H
 
-#include <asm/ioctl.h>                                                   
+#include <asm/ioctl.h>
 
-#define ACPI_THERMAL_MAGIC 's'                                                  
+#define ACPI_THERMAL_MAGIC 's'
 
 #define ACPI_THERMAL_GET_TRT_LEN	_IOR(ACPI_THERMAL_MAGIC, 1, unsigned long)
 #define ACPI_THERMAL_GET_ART_LEN	_IOR(ACPI_THERMAL_MAGIC, 2, unsigned long)
