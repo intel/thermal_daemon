3090fda175bb6919b2d730d5436e79a7b786ee32
Srinivas Pandruvada <Srinivas.pandruvada@linux.intel.com>
2019-07-10 11:52:37 -0700

    Add prefix for log messages
    ADD prefix like [INFO] [DEBUG] to log messages. This way easy
    to search imortant information.

--- src/main.cpp
+++ src/main.cpp
@@ -83,32 +83,43 @@ void thd_logger(const gchar *log_domain, GLogLevelFlags log_level,
 		const gchar *message, gpointer user_data) {
 	if (!(thd_log_level & log_level))
 		return;
-	if (thd_daemonize) {
-		int syslog_priority;
-		switch (log_level) {
-		case G_LOG_LEVEL_ERROR:
-			syslog_priority = LOG_CRIT;
-			break;
-		case G_LOG_LEVEL_CRITICAL:
-			syslog_priority = LOG_ERR;
-			break;
-		case G_LOG_LEVEL_WARNING:
-			syslog_priority = LOG_WARNING;
-			break;
-		case G_LOG_LEVEL_MESSAGE:
-			syslog_priority = LOG_NOTICE;
-			break;
-		case G_LOG_LEVEL_DEBUG:
-			syslog_priority = LOG_DEBUG;
-			break;
-		case G_LOG_LEVEL_INFO:
-		default:
-			syslog_priority = LOG_INFO;
-			break;
-		}
-		syslog(syslog_priority, "%s", message);
-	} else
-		g_print("%s", message);
+
+	int syslog_priority;
+	const char *prefix;
+
+	switch (log_level) {
+	case G_LOG_LEVEL_ERROR:
+		prefix = "[CRIT]";
+		syslog_priority = LOG_CRIT;
+		break;
+	case G_LOG_LEVEL_CRITICAL:
+		prefix = "[ERR]";
+		syslog_priority = LOG_ERR;
+		break;
+	case G_LOG_LEVEL_WARNING:
+		prefix = "[WARN]";
+		syslog_priority = LOG_WARNING;
+		break;
+	case G_LOG_LEVEL_MESSAGE:
+		prefix = "[MSG]";
+		syslog_priority = LOG_NOTICE;
+		break;
+	case G_LOG_LEVEL_DEBUG:
+		prefix = "[DEBUG]";
+		syslog_priority = LOG_DEBUG;
+		break;
+	case G_LOG_LEVEL_INFO:
+	default:
+		prefix = "[INFO]";
+		syslog_priority = LOG_INFO;
+		break;
+	}
+
+	if (thd_daemonize)
+		syslog(syslog_priority, "%s%s", prefix, message);
+	else
+		g_print("%s%s", prefix, message);
+
 }
 
 void clean_up_lockfile(void) {
