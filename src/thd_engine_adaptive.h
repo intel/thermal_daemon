c31212c22d032374d338a4f96cdcbc29ffe68488
Matthew Garrett <mjg59@google.com>
2020-04-10 14:28:09 -0700

    Add basic support for the DPTM adaptive power policy
    DPTM provides an adaptive power policy that allows for improved integration
    between the platform and the OS. Firmware can provide a set of complex
    conditions to the OS, and an OS agent (in this case thermal_daemon) is
    responsible for evaluating them, potentially making use of information that
    is easily available to the OS and not the firmware. The agent evaluates
    each set of conditions in turn, and once the first evaluates completely it
    triggers a set of actions.
    
    This implementation only covers a subset of the conditions - notably, it
    only currently handles conditions that are associated with OEM-specific
    variables exported via ACPI. It is also only currently capable of managing
    RAPL targets or loading new passive thermal policies, since that's all that
    my test system does.

---
+++ src/thd_engine_adaptive.h
@@ -0,0 +1,164 @@
+/*
+ * cthd_engine_adaptive.cpp: Adaptive thermal engine
+ *
+ * Copyright (C) 2013 Intel Corporation. All rights reserved.
+ * Copyright 2020 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 or later as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ *
+ * Author Name <Srinivas.Pandruvada@linux.intel.com>
+ * Author Name Matthew Garrett <mjg59@google.com>
+ *
+ */
+
+#ifndef THD_ENGINE_ADAPTIVE_H_
+#define THD_ENGINE_ADAPTIVE_H_
+
+#include "thd_engine_default.h"
+#include "thd_cpu_default_binding.h"
+#include "thd_adaptive_types.h"
+
+enum adaptive_condition {
+	Default = 0x01,
+	Orientation,
+	Proximity,
+	Motion,
+	Dock,
+	Workload,
+	Cooling_mode,
+	Power_source,
+	Aggregate_power_percentage,
+	Lid_state,
+	Platform_type,
+	Platform_SKU,
+	Utilisation,
+	TDP,
+	Duty_cycle,
+	Power,
+	Temperature,
+	Display_orientation,
+	Oem0,
+	Oem1,
+	Oem2,
+	Oem3,
+	Oem4,
+	Oem5,
+	PMAX,
+	PSRC,
+	ARTG,
+	CTYP,
+	PROP,
+	Battery_state,
+	Battery_rate,
+	Battery_remaining,
+	Battery_voltage,
+	PBSS,
+	Battery_cycles,
+	Battery_last_full,
+	Power_personality,
+	Screen_state,
+	AVOL,
+	ACUR,
+	AP01,
+	AP02,
+	AP10,
+	Time,
+	Temperature_without_hysteresis,
+	Mixed_reality,
+	User_presence,
+	RBHF,
+	VBNL,
+	CMPP,
+	Battery_percentage,
+	Battery_count,
+	Power_slider
+};
+
+enum adaptive_comparison {
+	ADAPTIVE_EQUAL = 0x01,
+	ADAPTIVE_LESSER_OR_EQUAL,
+	ADAPTIVE_GREATER_OR_EQUAL,
+};
+
+enum adaptive_operation {
+	AND = 0x01,
+	FOR
+};
+
+struct psvt {
+	std::string name;
+	std::string source;
+	std::string target;
+	int priority;
+	int sample_period;
+	int temp;
+	int domain;
+	int control_knob;
+	std::string limit;
+	int step_size;
+	int limit_coeff;
+	int unlimit_coeff;
+};
+
+struct condition {
+	enum adaptive_condition condition;
+	std::string device;
+        enum adaptive_comparison comparison;
+        int argument;
+	enum adaptive_operation operation;
+        enum adaptive_comparison time_comparison;
+        int time;
+	int target;
+	int state;
+	int state_entry_time;
+};
+
+class cthd_engine_adaptive: public cthd_engine_default {
+protected:
+	std::vector<ppcc_t> ppccs;
+	std::vector<std::vector<struct condition>> conditions;
+	std::vector<struct adaptive_target> targets;
+	std::vector<struct psvt> psvts;
+	std::string int3400_path;
+	uint64_t get_uint64(char *object, int *offset);
+	char *get_string(char *object, int *offset);
+	int parse_appc(char *appc, int len);
+	int parse_apat(char *apat, int len);
+	int parse_apct(char *apct, int len);
+	int parse_ppcc(char *name, char *ppcc, int len);
+	int parse_psvt(char *name, char *psvt, int len);
+	int parse_gddv(char *buf, int size);
+	struct psvt *find_psvt(std::string name);
+	void set_int3400_target(struct adaptive_target target);
+	int verify_condition(struct condition condition);
+	int verify_conditions();
+	int evaluate_condition(struct condition condition);
+	int evaluate_condition_set(std::vector<struct condition> condition_set);
+	int evaluate_conditions();
+	void execute_target(struct adaptive_target target);
+public:
+	cthd_engine_adaptive() :
+		cthd_engine_default("63BE270F-1C11-48FD-A6F7-3AF253FF3E2D") {
+	}
+
+	~cthd_engine_adaptive();
+	ppcc_t *get_ppcc_param(std::string name);
+	int thd_engine_start(bool ignore_cpuid_check);
+	void update_engine_state();
+};
+
+int thd_engine_create_adaptive_engine(bool ignore_cpuid_check);
+#endif /* THD_ENGINE_ADAPTIVE_H_ */
