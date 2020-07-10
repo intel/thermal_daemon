08c86c6c6b854dd82ba0d1df9611f1431c68eabb
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2020-07-10 14:23:54 -0700

    Add missing new line
    Add missing new line for "Passive 1" warning.

--- src/thd_trt_art_reader.cpp
+++ src/thd_trt_art_reader.cpp
@@ -167,7 +167,7 @@ int cthd_acpi_rel::generate_conf(std::string file_name) {
 	}
 
 	if (int3400.match_supported_uuid() != THD_SUCCESS) {
-		thd_log_info("Passive 1 UUID is not present, hence ignore _TRT, as it may have junk!!");
+		thd_log_info("Passive 1 UUID is not present, hence ignore _TRT, as it may have junk!!\n");
 		ret = -1;
 		goto cleanup;
 	}
