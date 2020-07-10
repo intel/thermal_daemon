5d7b2a136f7805cba7f45abbf91eba33c3a0154c
Bastien Nocera <hadess@hadess.net>
2020-07-10 12:40:21 +0200

    build: Run configure at the end of autogen.sh
    As is expected unless NO_CONFIGURE is set.

--- autogen.sh
+++ autogen.sh
@@ -9,3 +9,7 @@ cd "$srcdir"
 autoreconf --install --verbose
 
 cd "$olddir"
+
+if test -z "$NO_CONFIGURE"; then
+    $srcdir/configure "$@" && echo "Now type 'make' to compile `basename $srcdir`."
+fi
