9fd77a947d896baf5f0b32d5db858b0d10fd2477
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2022-06-29 17:22:18 -0700

    Add INT3400 base path for Raptor Lake
    Update the sysfs path for INT3400 sysfs to be able to run adaptive
    mode.

--- src/thd_engine_adaptive.cpp
+++ src/thd_engine_adaptive.cpp
@@ -1866,6 +1866,8 @@ int cthd_engine_adaptive::thd_engine_init(bool ignore_cpuid_check, bool adaptive
 		int3400_base_path = "/sys/bus/platform/devices/INTC1040:00/";
 	} else if (sysfs.exists("/sys/bus/platform/devices/INTC1041:00")) {
 		int3400_base_path = "/sys/bus/platform/devices/INTC1041:00/";
+	} else if (sysfs.exists("/sys/bus/platform/devices/INTC10A0:00")) {
+		int3400_base_path = "/sys/bus/platform/devices/INTC10A0:00/";
 	} else {
 		return THD_ERROR;
 	}
