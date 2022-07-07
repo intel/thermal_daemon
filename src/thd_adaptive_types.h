fe3375452fe95335bc23c8283a74236325a18730
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2022-07-07 14:37:03 -0700

    Add missing GPL2 header
    These files have no headers, so protex complains.

--- src/thd_adaptive_types.h
+++ src/thd_adaptive_types.h
@@ -1,3 +1,28 @@
+/*
+ * cthd_engine_adaptive.cpp: Adaptive thermal engine
+ *
+ * Copyright (C) 2013 Intel Corporation. All rights reserved.
+ * Copyright 2020 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 or later as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ *
+ * Author Name Matthew Garrett <mjg59@google.com>
+ *
+ */
+
 #ifndef THD_ADAPTIVE_TYPES_H_
 #define THD_ADAPTIVE_TYPES_H_
 
