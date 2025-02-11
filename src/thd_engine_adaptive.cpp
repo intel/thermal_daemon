intel/thermal_daemon

commit 3ea73d5fc4608e83b510f30b70153a299788cbc7

Auto detect int3400 base path
Instead of hardcoding for each CPU generation, detect path from
int3400 driver path.
Also remove duplicate processing in gddv and adpative classes,
by only setting during adaptive engine init.

src/thd_engine_adaptive.cpp
@@ -588,6 +588,31 @@ void cthd_engine_adaptive::update_engine_state() {
 	}
 }
 
+int cthd_engine_adaptive::set_int3400_base_path()
+{
+	const char *base_path = "/sys/bus/platform/drivers/int3400 thermal";
+	csys_fs sysfs ("");
+
+	if (sysfs.exists (base_path)) {
+		DIR *dir;
+		struct dirent *entry;
+
+		if ((dir = opendir (base_path)) != NULL) {
+			while ((entry = readdir (dir)) != NULL) {
+				if (!strncmp (entry->d_name, "INT", strlen ("INT"))) {
+					int3400_base_path = "/sys/bus/platform/devices/";
+					int3400_base_path += entry->d_name;
+					int3400_base_path += "/";
+					thd_log_debug ("Discovered int3400 path:%s\n", int3400_base_path.c_str ());
+					return THD_SUCCESS;
+				}
+			}
+		}
+	}
+
+	return THD_ERROR;
+}
+
 int cthd_engine_adaptive::thd_engine_init(bool ignore_cpuid_check,
 		bool adaptive) {
 	csys_fs sysfs("");
@@ -625,7 +650,8 @@ int cthd_engine_adaptive::thd_engine_init(bool ignore_cpuid_check,
 	} else if (sysfs.exists("/sys/bus/platform/devices/INTC1068:00")) {
 		int3400_base_path = "/sys/bus/platform/devices/INTC1068:00/";
 	} else {
-		return THD_ERROR;
+		if (set_int3400_base_path() != THD_SUCCESS)
+			return THD_ERROR;
 	}
 
 	if (sysfs.read(int3400_base_path + "firmware_node/path", int3400_path)
@@ -654,7 +680,7 @@ int cthd_engine_adaptive::thd_engine_init(bool ignore_cpuid_check,
 
 	delete[] buf;
 
-	res = gddv.gddv_init();
+	res = gddv.gddv_init(int3400_base_path);
 	if (res != THD_SUCCESS) {
 		return res;
 	}

src/thd_engine_adaptive.h
@@ -57,6 +57,7 @@ protected:
 	void exec_fallback_target(int target);
 	void execute_target(struct adaptive_target &target);
 	void install_passive_default();
+	int set_int3400_base_path();
 
 public:
 #ifndef ANDROID

src/thd_gddv.cpp
@@ -1619,7 +1619,7 @@ size_t cthd_gddv::gddv_load(char **buffer)
 
 #endif
 
-int cthd_gddv::gddv_init(void) {
+int cthd_gddv::gddv_init(std::string& base_path) {
 	csys_fs sysfs("");
 	char *buf;
 	size_t size;
@@ -1630,21 +1630,7 @@ int cthd_gddv::gddv_init(void) {
 		goto skip_load;
 	}
 
-	if (sysfs.exists("/sys/bus/platform/devices/INT3400:00")) {
-		int3400_base_path = "/sys/bus/platform/devices/INT3400:00/";
-	} else if (sysfs.exists("/sys/bus/platform/devices/INTC1040:00")) {
-		int3400_base_path = "/sys/bus/platform/devices/INTC1040:00/";
-	} else if (sysfs.exists("/sys/bus/platform/devices/INTC1041:00")) {
-		int3400_base_path = "/sys/bus/platform/devices/INTC1041:00/";
-	} else if (sysfs.exists("/sys/bus/platform/devices/INTC10A0:00")) {
-		int3400_base_path = "/sys/bus/platform/devices/INTC10A0:00/";
-	} else if (sysfs.exists("/sys/bus/platform/devices/INTC1042:00")) {
-		int3400_base_path = "/sys/bus/platform/devices/INTC1042:00/";
-	} else if (sysfs.exists("/sys/bus/platform/devices/INTC1068:00")) {
-		int3400_base_path = "/sys/bus/platform/devices/INTC1068:00/";
-	} else {
-		return THD_ERROR;
-	}
+	int3400_base_path = base_path;
 
 	if (sysfs.read(int3400_base_path + "firmware_node/path",
 			int3400_path) < 0) {

src/thd_gddv.h
@@ -260,7 +260,7 @@ public:
 	std::vector<struct adaptive_target> targets;
 
 	ppcc_t* get_ppcc_param(std::string name);
-	int gddv_init(void);
+	int gddv_init(std::string& base_path);
 	size_t gddv_load(char **buf);
 	void gddv_free(void);
 	int verify_conditions();

commit c72fb4f7bdb65f64603c6ebdcf0e132b1f6b8080

Fix resource leak on opened dir descriptor
The opened dir descriptor is not being closed and is causing
a resource leak. Close it.

Fixes: 3ea73d5fc460 ("Auto detect int3400 base path")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

src/thd_engine_adaptive.cpp
@@ -607,6 +607,7 @@ int cthd_engine_adaptive::set_int3400_base_path()
 					return THD_SUCCESS;
 				}
 			}
+			closedir(dir);
 		}
 	}
 