212ebcde0216eabe8f0d0d2fd61367800e84ae65
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2025-07-29 10:41:58 -0700

    Add Wildcat Lake to support list
    Add CPU model of Wildcat Lake.
    
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

--- src/thd_engine.cpp
+++ src/thd_engine.cpp
@@ -780,6 +780,7 @@ static const supported_ids_t id_table[] = {
 		{ 6, 0xc5 }, // Arrow Lake H
 		{ 6, 0xb5 }, // Arrow Lake U
 		{ 6, 0xcc }, // Panther Lake L
+		{ 6, 0xd5 }, // Wildcat Lake L
 		{ 0, 0 } // Last Invalid entry
 };
 
