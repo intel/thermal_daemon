intel/thermal_daemon

commit d74be68511d556280fcf072f6f73a2e53a8ed07a

Check for start path instead of anywhere
When searching for "/sys/" path, check from start instead of anywhere.
Also check during adding a new custom sensor.

Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

src/thd_engine.cpp
@@ -1099,6 +1099,15 @@ void cthd_engine::check_for_rt_kernel() {
 int cthd_engine::user_add_sensor(std::string name, std::string path) {
 	pthread_mutex_lock(&thd_engine_mutex);
 
+	if (path.empty())
+		return THD_ERROR;
+
+	std::string start("/sys/");
+	if (path.substr(0, start.length()) != start) {
+		thd_log_debug("Invalid path %s\n", path.c_str());
+		return THD_ERROR;
+	}
+
 	for (unsigned int i = 0; i < sensors.size(); ++i) {
 		if (sensors[i]->get_sensor_type() == name) {
 			cthd_sensor *sensor = sensors[i].get();

src/thd_cdev_gen_sysfs.cpp
@@ -27,17 +27,18 @@
 int cthd_gen_sysfs_cdev::update() {
 	if (cdev_sysfs.exists()) {
 		std::string base_path = cdev_sysfs.get_base_path();
-		if (base_path.find("/sys/") != std::string::npos) {
-			int ret = cdev_sysfs.read("", &curr_state);
-			if (ret < 0)
-				return ret;
-
-			min_state = max_state = curr_state;
-		} else {
-			thd_log_info( "cthd_gen_sysfs_cdev::update: Invalid sysfs path or allowed path %s\n",
+		std::string start("/sys/");
+		if (base_path.substr(0, start.length()) != start) {
+			thd_log_debug( "cthd_gen_sysfs_cdev::update: Invalid sysfs path or allowed path %s\n",
 					base_path.c_str());
 			return THD_ERROR;
 		}
+
+		int ret = cdev_sysfs.read("", &curr_state);
+		if (ret < 0)
+			return ret;
+
+		min_state = max_state = curr_state;
 	} else {
 		thd_log_info( "cthd_gen_sysfs_cdev::update: sysfs path does not exist %s\n",
 				cdev_sysfs.get_base_path().c_str());

src/thd_engine_default.cpp
@@ -195,7 +195,7 @@ int cthd_engine_default::read_thermal_sensors() {
 			} else {
 				std::unique_ptr<cthd_sensor> sensor_new;
 				if (sensor_config->virtual_sensor) {
-				std::unique_ptr<cthd_sensor_virtual> sensor_virt(new cthd_sensor_virtual(
+					std::unique_ptr<cthd_sensor_virtual> sensor_virt(new cthd_sensor_virtual(
 							index, sensor_config->name,
 							sensor_config->sensor_link.name,
 							sensor_config->sensor_link.multiplier,
@@ -205,6 +205,13 @@ int cthd_engine_default::read_thermal_sensors() {
 					}
 					sensor_new = std::move(sensor_virt);
 				} else {
+					std::string start("/sys/");
+					if (sensor_config->path.substr(0, start.length()) != start) {
+						thd_log_debug( "Invalid sysfs path or allowed path %s\n",
+								sensor_config->path.c_str());
+						continue;
+					}
+
 					sensor_new.reset(new cthd_sensor(index, sensor_config->path,
 							sensor_config->name, SENSOR_TYPE_RAW));
 					if (sensor_new->sensor_update() != THD_SUCCESS) {

src/thd_sensor.h
@@ -69,6 +69,11 @@ public:
 	int set_threshold(int index, int temp);
 	;
 	void update_path(std::string str) {
+		std::string start("/sys/");
+		if (str.substr(0, start.length()) != start) {
+			thd_log_debug("Invalid path %s\n", str.c_str());
+			return;
+		}
 		sensor_sysfs.update_path(std::move(str));
 	}
 	void set_async_capable(bool capable) {

commit 54ee8b84d3d974030d504e35fc70a51bbabcfc34

Fix return without unlocking thd_engine_mutex
A recent set of checks on path don't unlock thd_engine_mutex on error
return exits from cthd_engine::user_add_senso leading to locking issues.

Fix this by performing the lock on thd_engine_mutex after the sanity
checks on path to avoid the locking issue and to defer locking to
the latest point where it is required.

Fixes: d74be68511d55 ("Check for start path instead of anywhere")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

src/thd_engine.cpp
@@ -1010,10 +1010,8 @@ void cthd_engine::check_for_rt_kernel() {
 }
 
 int cthd_engine::user_add_sensor(std::string name, std::string path) {
-	pthread_mutex_lock(&thd_engine_mutex);
-
 	if (path.empty())
-		return THD_ERROR;
+		pthread_mutex_unlock(&thd_engine_mutex);
 
 	std::string start("/sys/");
 	if (path.substr(0, start.length()) != start) {
@@ -1021,6 +1019,7 @@ int cthd_engine::user_add_sensor(std::string name, std::string path) {
 		return THD_ERROR;
 	}
 
+	pthread_mutex_lock(&thd_engine_mutex);
 	for (unsigned int i = 0; i < sensors.size(); ++i) {
 		if (sensors[i]->get_sensor_type() == name) {
 			cthd_sensor *sensor = sensors[i].get();