f31c46022daf8b386f9cbdf03d4789a87f0e83a9
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2026-08-14 14:10:32 -0700

    optional gtkdocsize
    By default gtkdoc dependency is not there unless called with autogen.sh
    --enable-doc.
    
    Issue #407
    
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

--- autogen.sh
+++ autogen.sh
@@ -4,10 +4,24 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 olddir=`pwd`
 
+enable_doc=no
+for arg in "$@"; do
+    case "$arg" in
+        --enable-doc|--enable-doc=yes|--enable-gtk-doc|--enable-gtk-doc=yes)
+            enable_doc=yes
+            ;;
+    esac
+done
+
 cd "$srcdir"
 
 aclocal --install || exit 1
-gtkdocize --copy --flavour no-tmpl || exit 1
+if test "x$enable_doc" = "xyes"; then
+    echo "autogen.sh: gtk-doc enabled; running gtkdocize"
+    gtkdocize --copy --flavour no-tmpl || exit 1
+else
+    echo "autogen.sh: gtk-doc disabled; skipping gtkdocize"
+fi
 autoreconf --install --verbose || exit 1
 
 cd "$olddir"
